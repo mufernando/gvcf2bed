63e5a139d1f11cff0115be96f531726308cff959
Sander Bollen <sander@sndrtj.eu>
2016-08-24 18:04:27 +0200

    full description (readme) in setup

--- setup.py
+++ setup.py
@@ -7,15 +7,22 @@ setup.py
 :copyright: (c) 2016 Leiden University Medical Center
 :license: MIT
 """
+from os.path import abspath, dirname, join
 
 from setuptools import setup
+
+readme_file = join(abspath(dirname(__file__)), "README.md")
+with open(readme_file) as desc_handle:
+    long_desc = desc_handle.read()
 
 setup(
     name="gvcf2bed",
     version="0.1",
     description="Convert gVCF into BED",
+    long_description=long_desc,
     author="Sander Bollen",
     author_email="a.h.b.bollen@lumc.nl",
+    url="https://github.com/sndrtj/gvcf2bed",
     license="MIT",
     packages=["gvcf2bed"],
     install_requires=["pyvcf==0.6.8"],
